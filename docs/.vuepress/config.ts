1e562a6000ab760dbbe3a332e09fcf24da560342
ikimukti <iki.mukti@gmail.com>
2025-08-16 08:24:04 +0700

    chore: add Microsoft site verification meta tag to config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -236,6 +236,7 @@ export default defineUserConfig({
   head: [
     ['link', { rel: 'icon', type: 'image/png', href: '/image/favicon-48x48.png' }],
     ['meta', { name: 'algolia-site-verification', content: '37BAF5981A83D338' }],
+    ['meta', { name: 'msvalidate.01', content: '4077B11CAE89D53B7DAE1D82710447B0' }],
   ],
 
   bundler: viteBundler(),

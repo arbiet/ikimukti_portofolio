8b7429605a8129569846911a7072d4d6f2dce298
ikimukti <iki.mukti@gmail.com>
2025-08-16 17:51:25 +0700

    chore: remove favicon link from configuration

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -239,7 +239,6 @@ export default defineUserConfig({
     ['meta', { name: 'yandex-verification', content: 'b7d4fe28c3d29dd2' }],
     ['meta', { name: 'naver-site-verification', content: '70ef6d5bfc1bba6db665b2fdba6ab2afd49f993f' }],
     ['meta', { name: 'p:domain_verify', content: 'bfa2c54f2e4986e4956c14d4fe581412' }],
-    ['link', { rel: 'icon', href: '/favicon-48x48.png' }],
     ['script', { async: true, src: 'https://news.google.com/swg/js/v1/swg-basic.js' , type: 'application/javascript' }],
     [
       'script',
